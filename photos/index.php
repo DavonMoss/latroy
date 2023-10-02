39d506dfec98cfbb2b3502941528068fbbdb23d7
davon moss <davonlatroymoss@gmail.com>
2023-10-02 13:58:21 -0500

    basic functionality for photos page, need to implement randomization

--- photos/index.php
+++ photos/index.php
@@ -15,6 +15,28 @@
           <img src="./../images/win95pc.png" alt="pretty computer" width=30% height=30%>
         </a>
       </div>
+  
+      <!-- IMAGES -->
+      <div class="pics">
+        <?php
+          $db_connection = mysqli_connect("127.0.0.1", "latroy_admin", "admin", "latroy_net", "3306");
+
+          if(!$db_connection) {
+            die("mission failed: " . mysqli_connect_error());
+          } else {
+            $pictures_query = mysqli_query($db_connection, "select * from photos");
+            #generate 5 random numbers within the range of the PIDs and pick them out to display
+            #echo out img tags with those photos as the src
+
+            while($pic = mysqli_fetch_assoc($pictures_query)) {
+              echo '<img src="';
+              echo $pic['photo_file_path'];
+              echo '" alt="picture">';
+            }
+          }
+        ?>
+      </div>
     </div>
+
   </body>
 </html>
